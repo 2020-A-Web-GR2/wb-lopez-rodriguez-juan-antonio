555a95711da3cc2cb209849985f52c3af43c9652
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-07-03 16:05:57 -0500

    app.module.ts
    decoradores para codigos de estado, cabeceras y sumar numeros

---
+++ 01-http/introduccion/src/http/http-juego.controller.ts
@@ -0,0 +1,46 @@
+import {BadRequestException, Controller, Delete, Get, Header, HttpCode, Param, Post} from "@nestjs/common";
+
+@Controller('juegos-http')
+export class HttpJuegoController{
+
+    @Get('hola')
+    @HttpCode(201)
+    hola(){
+        throw new BadRequestException('No envia nada')
+        //return 'Hola GET! :)'
+    }
+
+    @Post('hola')
+    @HttpCode(202)
+    holaPost(){
+        return 'Hola POST'
+    }
+
+    @Delete('hola')
+    @HttpCode(204)
+    @Header('Cache-control', 'none')
+    @Header('EPN', 'probando las cosas')
+    holaDelete(){
+        return 'Hola DELETE'
+    }
+
+    // http:localhost:3001/juegos-http/parametros-ruta/XX/gestion/YY
+    @Get('/parametros-ruta/:edad/gestion/:altura')
+    parametrosRutaEjemplo(
+        @Param() parametrosRuta
+    ){
+        console.log('Parametros', parametrosRuta);
+        // Validar que la edad y la altura son numeros
+        //isNaN(parametrosRuta.edad)   //'ABC' true
+        //isNaN(parametrosRuta.altura)   //'ABC' true
+        //throw new BadRequestException('No son Numeros')
+
+        const edad = Number(parametrosRuta.edad);
+        const altura = Number(parametrosRuta.altura);
+        return edad + altura;
+        //return 'ok';
+    }
+
+
+
+}
