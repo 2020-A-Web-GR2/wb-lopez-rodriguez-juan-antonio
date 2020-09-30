663f47812127271c9155a331fdcc54d8553a1064
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-09-30 16:17:24 -0500

    vista login e inicio con componentes

--- 01-http/examen2b/src/cancion/cancion.controller.ts
+++ 01-http/examen2b/src/cancion/cancion.controller.ts
@@ -6,24 +6,24 @@ import {
     InternalServerErrorException,
     NotFoundException,
     Param,
-    Post, Put
+    Post, Put, Res
 } from "@nestjs/common";
 import {CancionService} from "./cancion.service";
 
 @Controller('cancion')
-export class CancionController{
+export class CancionController {
 
     constructor(
-        private readonly _cancionService:CancionService
+        private readonly _cancionService: CancionService
     ) {
     }
 
     @Get()
-    async mostrarTodosCanciones(){
-        try{
+    async mostrarTodosCanciones() {
+        try {
             const respuesta = await this._cancionService.buscarTodasCanciones();
-            return  respuesta;
-        }catch (e) {
+            return respuesta;
+        } catch (e) {
             console.log(e);
             throw new InternalServerErrorException({
                 mensaje: 'Error del servidor'
@@ -35,21 +35,21 @@ export class CancionController{
     @Get(':id')
     async verUnaCancion(
         @Param() parametrosRuta
-    ){
+    ) {
         let respuesta;
-        try{
+        try {
             respuesta = await this._cancionService.buscarUnaCancion(Number(parametrosRuta.id));
-        }catch (e) {
+        } catch (e) {
             console.log(e)
             throw new BadRequestException({
                 mensaje: 'Error validando datos'
             })
         }
-        if(respuesta){
+        if (respuesta) {
             return respuesta;
-        }else{
+        } else {
             throw new NotFoundException({
-                mensaje:'No existen registros'
+                mensaje: 'No existen registros'
             })
         }
     }
@@ -57,10 +57,10 @@ export class CancionController{
     @Post()
     async crearUnaCancion(
         @Body() parametrosCuerpo
-    ){
-        try{
+    ) {
+        try {
             const respuesta = await this._cancionService.crearUnaCancion(parametrosCuerpo);
-        }catch (e) {
+        } catch (e) {
             console.log(e)
             throw new BadRequestException({
                 mensaje: 'Error validando datos'
@@ -72,15 +72,15 @@ export class CancionController{
     async editarUnaCancion(
         @Param() parametrosRuta,
         @Body() parametrosCuerpo
-    ){
+    ) {
         const id = Number(parametrosRuta.id);
         const cancionEditada = parametrosCuerpo;
         cancionEditada.id = id;
         console.log('CancionEditada', cancionEditada);
-        try{
+        try {
             const respuesta = await this._cancionService.editarUnaCancion(cancionEditada);
             return respuesta;
-        }catch (e) {
+        } catch (e) {
             console.log(e)
             throw new BadRequestException({
                 mensaje: 'Error validando datos'
@@ -91,19 +91,60 @@ export class CancionController{
     @Delete(':id')
     async eliminarUnaCancion(
         @Param() parametrosRuta
-    ){
+    ) {
         const id = Number(parametrosRuta.id)
-        try{
+        try {
             const respuesta = await this._cancionService.eliminarUnaCancion(id);
             return {
                 mensaje: 'Registro con id ' + id + ' eliminado'
             }
-        }catch (e) {
+        } catch (e) {
             console.log(e)
             throw new BadRequestException({
                 mensaje: 'Error validando datos'
             })
         }
     }
+
+    @Get('vista/cancion')
+    vistaCancion(
+        @Res() res
+    ) {
+        res.render(
+            'inicio',
+            )
+    }
+    @Get('vista/login')
+    vistaLogin(
+        @Res() res
+    ) {
+        res.render(
+            'login',
+        )
+    }
+
+
+
+
+    // async vistaCancion(
+    //     @Res() res
+    // ){
+    //     let resultadoEncotrado
+    //     try{
+    //         resultadoEncotrado = await this._cancionService.buscarTodasCanciones();
+    //     }catch (e) {
+    //         throw new InternalServerErrorException('Error encontrando canciones')
+    //     }
+    //     if(resultadoEncotrado){
+    //         res.render(
+    //             'cancion/inicioCancion',
+    //             {
+    //                 arregloCanciones: resultadoEncotrado
+    //             })
+    //     }else{
+    //         throw new NotFoundException('No se encontraron canciones')
+    //     }
+    // }
+
 
 }
