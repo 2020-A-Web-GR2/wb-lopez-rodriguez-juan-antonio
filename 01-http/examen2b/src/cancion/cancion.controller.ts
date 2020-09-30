79b717d3d40cc820b3f91a40e9ff1519523e1eba
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-09-30 15:43:56 -0500

    restfull completo

--- 01-http/examen2b/src/cancion/cancion.controller.ts
+++ 01-http/examen2b/src/cancion/cancion.controller.ts
@@ -1,12 +1,12 @@
 import {
     BadRequestException,
     Body,
-    Controller,
+    Controller, Delete,
     Get,
     InternalServerErrorException,
     NotFoundException,
     Param,
-    Post
+    Post, Put
 } from "@nestjs/common";
 import {CancionService} from "./cancion.service";
 
@@ -54,15 +54,50 @@ export class CancionController{
         }
     }
 
-
-
-
     @Post()
     async crearUnaCancion(
         @Body() parametrosCuerpo
     ){
         try{
             const respuesta = await this._cancionService.crearUnaCancion(parametrosCuerpo);
+        }catch (e) {
+            console.log(e)
+            throw new BadRequestException({
+                mensaje: 'Error validando datos'
+            })
+        }
+    }
+
+    @Put(':id')
+    async editarUnaCancion(
+        @Param() parametrosRuta,
+        @Body() parametrosCuerpo
+    ){
+        const id = Number(parametrosRuta.id);
+        const cancionEditada = parametrosCuerpo;
+        cancionEditada.id = id;
+        console.log('CancionEditada', cancionEditada);
+        try{
+            const respuesta = await this._cancionService.editarUnaCancion(cancionEditada);
+            return respuesta;
+        }catch (e) {
+            console.log(e)
+            throw new BadRequestException({
+                mensaje: 'Error validando datos'
+            })
+        }
+    }
+
+    @Delete(':id')
+    async eliminarUnaCancion(
+        @Param() parametrosRuta
+    ){
+        const id = Number(parametrosRuta.id)
+        try{
+            const respuesta = await this._cancionService.eliminarUnaCancion(id);
+            return {
+                mensaje: 'Registro con id ' + id + ' eliminado'
+            }
         }catch (e) {
             console.log(e)
             throw new BadRequestException({
